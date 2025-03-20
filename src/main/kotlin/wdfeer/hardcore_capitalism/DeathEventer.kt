56b9f0a5bc432d9fc301c09b53b618bd57e5cbb8
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-03-20 07:39:21 +0100

    Implement banning the poor

--- src/main/kotlin/wdfeer/hardcore_capitalism/DeathEventer.kt
+++ src/main/kotlin/wdfeer/hardcore_capitalism/DeathEventer.kt
@@ -1,5 +1,26 @@
 package wdfeer.hardcore_capitalism
 
+import com.glisco.numismaticoverhaul.currency.CurrencyHelper
+import com.mojang.authlib.GameProfile
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents
+import net.minecraft.server.BannedPlayerEntry
+
+private const val MONEY_TAKEN = 500L // todo make config
+
 fun registerOnDeathEvent() {
-    TODO()
+    ServerPlayerEvents.AFTER_RESPAWN.register { oldPlayer, newPlayer, alive ->
+        val gotMoney = CurrencyHelper.deductFromInventory(newPlayer, MONEY_TAKEN)
+        if (!gotMoney) {
+            val manager = newPlayer.server.playerManager
+            manager.remove(newPlayer)
+            manager.userBanList.add(
+                BannedPlayerEntry(
+                    GameProfile(
+                        newPlayer.uuid,
+                        newPlayer.name.string
+                    )
+                )
+            )
+        }
+    }
 }
--- src/main/kotlin/wdfeer/hardcore_capitalism/Numismatics.kt
+++
@@ -1,7 +0,0 @@
-package wdfeer.hardcore_capitalism
-
-import com.glisco.numismaticoverhaul.currency.CurrencyHelper
-import net.minecraft.server.network.ServerPlayerEntity
-
-fun getMoney(player: ServerPlayerEntity): Long
-    = CurrencyHelper.getMoneyInInventory(player, false)
