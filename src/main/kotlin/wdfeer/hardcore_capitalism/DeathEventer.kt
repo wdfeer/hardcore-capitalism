cfafd9e77344194827e67ec069a7e78c2163da21
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-03-20 08:02:25 +0100

    Add todo

--- src/main/kotlin/wdfeer/hardcore_capitalism/DeathEventer.kt
+++ src/main/kotlin/wdfeer/hardcore_capitalism/DeathEventer.kt
@@ -4,15 +4,17 @@ import com.glisco.numismaticoverhaul.currency.CurrencyHelper
 import com.mojang.authlib.GameProfile
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents
 import net.minecraft.server.BannedPlayerEntry
+import net.minecraft.text.Text
 
 private const val MONEY_TAKEN = 500L // todo make config
 
 fun registerOnDeathEvent() {
-    ServerPlayerEvents.AFTER_RESPAWN.register { oldPlayer, newPlayer, alive ->
-        val gotMoney = CurrencyHelper.deductFromInventory(newPlayer, MONEY_TAKEN)
+    ServerPlayerEvents.AFTER_RESPAWN.register { _, newPlayer, _ ->
+        val gotMoney = CurrencyHelper.deductFromInventory(newPlayer, MONEY_TAKEN) // todo: this doesn't check the 'purse', only coins in inventory
         if (!gotMoney) {
             val manager = newPlayer.server.playerManager
-            manager.remove(newPlayer)
+
+            newPlayer.networkHandler.disconnect(Text.of("Try being less poor next time!"))
             manager.userBanList.add(
                 BannedPlayerEntry(
                     GameProfile(
